2265ed3f9a6bb9c6cb6c338c32b06ce837f4994a
Keith Packard <keithp@keithp.com>
2025-04-13 18:22:52 -0700

    include/zephyr: Avoid undefined behavior in sys_bitops.h
    Use unsigned types for all bit checking to avoid signed integer overflow
    when shifting left.
    
    Make sure sys_test_bit returns a value which fits in 'int' by computing a
    boolean instead of returning the and'ed bit.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- include/zephyr/arch/common/sys_bitops.h
+++ include/zephyr/arch/common/sys_bitops.h
@@ -25,21 +25,21 @@ static ALWAYS_INLINE void sys_set_bit(mem_addr_t addr, unsigned int bit)
 {
 	uint32_t temp = *(volatile uint32_t *)addr;
 
-	*(volatile uint32_t *)addr = temp | (1 << bit);
+	*(volatile uint32_t *)addr = temp | ((uint32_t) 1 << bit);
 }
 
 static ALWAYS_INLINE void sys_clear_bit(mem_addr_t addr, unsigned int bit)
 {
 	uint32_t temp = *(volatile uint32_t *)addr;
 
-	*(volatile uint32_t *)addr = temp & ~(1 << bit);
+	*(volatile uint32_t *)addr = temp & ~((uint32_t) 1 << bit);
 }
 
 static ALWAYS_INLINE int sys_test_bit(mem_addr_t addr, unsigned int bit)
 {
 	uint32_t temp = *(volatile uint32_t *)addr;
 
-	return temp & (1 << bit);
+	return (temp & ((uint32_t) 1 << bit)) != 0;
 }
 
 static ALWAYS_INLINE void sys_set_bits(mem_addr_t addr, unsigned int mask)
